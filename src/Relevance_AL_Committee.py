95f247f9984bac5a3595b2044cc891456afcc000
Yunyi Huang <yuh022@ucsd.edu>
2023-03-13 17:20:04 -0700

    final code

--- src/Relevance_AL_Committee.py
+++ src/Relevance_AL_Committee.py
@@ -8,67 +8,76 @@ def trial_relevance(df, model_names, training_method, balance, sampling_size, so
     output['sampling_size'] = sampling_size
     output['sort_by_time'] = sort_by_time
     output['partition_ratio'] = partition_ratio
+    accuracy_lst, f1_lst, precision_lst, recall_lst, specificity_lst = [], [], [], [], []
+
+    for i in range(5):  
+        # 1. Balance dataset 
+        df_1, df_2_3 = df[df.Bucket=='1'], df[df.Bucket=='2 or 3']
+        df_lst = [df_1, df_2_3]
         
-    # 1. Balance dataset 
-    df_1, df_2_3 = df[df.Bucket=='1'], df[df.Bucket=='2 or 3']
-    df_lst = [df_1, df_2_3]
-    
-    # 1.1 Balance the label distribution  (50% Bucket 1 vs. 50% Non-Bucket 1)
-    if balance:
-        sample_size = min(df_1.shape[0], df_2_3.shape[0])
-        if df_1.shape[0] > sample_size:
-            df_1 = resample(df_1, replace=False, n_samples=sample_size, random_state=random_state)
-        if df_2_3.shape[0] > sample_size:
-            df_2_3 = resample(df_2_3, replace=False, n_samples=sample_size, random_state=random_state)
+        # 1.1 Balance the label distribution  (50% Bucket 1 vs. 50% Non-Bucket 1)
+        if balance:
+            sample_size = min(df_1.shape[0], df_2_3.shape[0])
+            if df_1.shape[0] > sample_size:
+                df_1 = resample(df_1, replace=False, n_samples=sample_size, random_state=random_state)
+            if df_2_3.shape[0] > sample_size:
+                df_2_3 = resample(df_2_3, replace=False, n_samples=sample_size, random_state=random_state)
+                
+        # 1.2 Keep the natural label distribution
+        seed_1, unlabeled_1, test_1 = partition_data(df_1, partition_ratio, sort_by_time)
+        seed_2_3, unlabeled_2_3, test_2_3 = partition_data(df_2_3, partition_ratio, sort_by_time)
+        seed, unlabeled, test = pd.concat([seed_1, seed_2_3]), pd.concat([unlabeled_1, unlabeled_2_3]), pd.concat([test_1, test_2_3])
+        output['seed_size'], output['unlabeled_size'], output['test_size'] = seed.shape[0], unlabeled.shape[0], test.shape[0]
+        
+        initial_seed = seed.copy()
+        initial_unlabeled = unlabeled.copy()
+        
+        # 2. Train the model
+        initial_pipelines, initial_accuracy = train_model(initial_seed,1)
+        
+        # 3. Active Learning
+        if sampling_size == 0:
+            pipelines, accuracy = initial_pipelines, initial_accuracy
             
-    # 1.2 Keep the natural label distribution
-    seed_1, unlabeled_1, test_1 = partition_data(df_1, partition_ratio, sort_by_time)
-    seed_2_3, unlabeled_2_3, test_2_3 = partition_data(df_2_3, partition_ratio, sort_by_time)
-    seed, unlabeled, test = pd.concat([seed_1, seed_2_3]), pd.concat([unlabeled_1, unlabeled_2_3]), pd.concat([test_1, test_2_3])
-    output['seed_size'], output['unlabeled_size'], output['test_size'] = seed.shape[0], unlabeled.shape[0], test.shape[0]
-    
-    initial_seed = seed.copy()
-    initial_unlabeled = unlabeled.copy()
-    
-    # 2. Train the model
-    initial_pipelines, initial_accuracy = train_model(initial_seed,1)
-    
-    # 3. Active Learning
-    if sampling_size == 0:
-        pipelines, accuracy = initial_pipelines, initial_accuracy
-        
-    # 3.1 Initial Model + Random Sampling
-    elif training_method == 'random_sampling':
-        if initial_unlabeled.shape[0] >= sampling_size:
-            sample_unlabeled = initial_unlabeled.sample(n=sampling_size, replace=False, random_state=random_state)
+        # 3.1 Initial Model + Random Sampling
+        elif training_method == 'random_sampling':
+            if initial_unlabeled.shape[0] >= sampling_size:
+                sample_unlabeled = initial_unlabeled.sample(n=sampling_size, replace=False, random_state=random_state)
+            else:
+                sample_unlabeled = initial_unlabeled.sample(n=sampling_size, replace=True, random_state=random_state)
+            seed_and_sample_unlabeled_df = pd.concat([initial_seed, sample_unlabeled])
+            pipelines, accuracy = train_model(seed_and_sample_unlabeled_df,1)
+            
+        # 3.2 Initial Model + Active Learning
         else:
-            sample_unlabeled = initial_unlabeled.sample(n=sampling_size, replace=True, random_state=random_state)
-        seed_and_sample_unlabeled_df = pd.concat([initial_seed, sample_unlabeled])
-        pipelines, accuracy = train_model(seed_and_sample_unlabeled_df,1)
-        
-    # 3.2 Initial Model + Active Learning
-    else:
-        pipelines, accuracy = active_learning(initial_pipelines, initial_seed, initial_unlabeled, sampling_size, 1)
+            pipelines, accuracy = active_learning(initial_pipelines, initial_seed, initial_unlabeled, sampling_size, 1)
 
-    # 4. Report Model Accuracy
-    X_test, Y_test = test[['text_cleaned']], test[['Bucket']]
-    
-
-    pl_preds = []
-    for pl in pipelines:
-        # compute the testing accuracy
-        prediction = pl.predict(pd.DataFrame(X_test))
-        pl_preds.append([pl, prediction])
+        # 4. Report Model Accuracy
+        X_test, Y_test = test[['text_cleaned']], test[['Bucket']]
         
-    #Saves all the model predictions
-    all_preds = np.array([x[1] for x in pl_preds]).transpose()
-    #Find the mode in all preds
-    prediction = [mode(i) for i in all_preds]
-    accuracy = round(accuracy_score(Y_test, prediction), round_number)
-    f1 = round(f1_score(np.array(Y_test), prediction, pos_label='1'), round_number)
-    precision = round(precision_score(np.array(Y_test), prediction, pos_label='1', average='binary'), round_number)
-    recall = round(recall_score(np.array(Y_test), prediction, pos_label='1', average='binary'), round_number)
-    specificity = round(recall_score(np.array(Y_test), prediction, pos_label='2 or 3', average='binary'), round_number)
+
+        pl_preds = []
+        for pl in pipelines:
+            # compute the testing accuracy
+            prediction = pl.predict(pd.DataFrame(X_test))
+            pl_preds.append([pl, prediction])
+            
+        #Saves all the model predictions
+        all_preds = np.array([x[1] for x in pl_preds]).transpose()
+        #Find the mode in all preds
+        prediction = [mode(i) for i in all_preds]
+        accuracy = round(accuracy_score(Y_test, prediction), round_number)
+        f1 = round(f1_score(np.array(Y_test), prediction, pos_label='1'), round_number)
+        precision = round(precision_score(np.array(Y_test), prediction, pos_label='1', average='binary'), round_number)
+        recall = round(recall_score(np.array(Y_test), prediction, pos_label='1', average='binary'), round_number)
+        specificity = round(recall_score(np.array(Y_test), prediction, pos_label='2 or 3', average='binary'), round_number)
+
+        accuracy_lst.append(accuracy)
+        f1_lst.append(f1)
+        precision_lst.append(precision) 
+        recall_lst.append(recall) 
+        specificity_lst.append(specificity) 
+
     output['accuracy'] = accuracy
     output['f1_score'] = f1
     output['precision'] = precision
--- src/Sentiment_AL_Committee.py
+++ src/Sentiment_AL_Committee.py
@@ -8,76 +8,92 @@ def trial_sentiment(df, model_names, training_method, balance, sampling_size, so
     output['sampling_size'] = sampling_size
     output['sort_by_time'] = sort_by_time
     output['partition_ratio'] = partition_ratio
+    accuracy_lst = []
+    f1_micro_lst, f1_macro_lst, f1_weighted_lst = [], [], []
+    precision_micro_lst, precision_macro_lst, precision_weighted_lst = [], [], []
+    recall_micro_lst, recall_macro_lst, recall_weighted_lst = [], [], []   
         
-    # 1. Balance dataset 
-    df_1, df_2, df_3 = df[df.SentimentScore=='Negative'], df[df.SentimentScore=='Neutral'], df[df.SentimentScore=='Positive']
+    for i in range(5):
+        # 1. Balance dataset 
+        df_1, df_2, df_3 = df[df.SentimentScore=='Negative'], df[df.SentimentScore=='Neutral'], df[df.SentimentScore=='Positive']
 
-    # 1.1 Balance the label distribution  (33% Negative vs. 33% Neutral vs. 33% Positive)
-    if balance:
-        sample_size = min(df_1.shape[0], df_2.shape[0], df_3.shape[0])
-        if df_1.shape[0] > sample_size:
-            df_1 = resample(df_1, replace=False, n_samples=sample_size, random_state=random_state)
-        if df_2.shape[0] > sample_size:
-            df_2 = resample(df_2, replace=False, n_samples=sample_size, random_state=random_state)
-        if df_3.shape[0] > sample_size:
-            df_3 = resample(df_3, replace=False, n_samples=sample_size, random_state=random_state)
+        # 1.1 Balance the label distribution  (33% Negative vs. 33% Neutral vs. 33% Positive)
+        if balance:
+            sample_size = min(df_1.shape[0], df_2.shape[0], df_3.shape[0])
+            if df_1.shape[0] > sample_size:
+                df_1 = resample(df_1, replace=False, n_samples=sample_size, random_state=random_state)
+            if df_2.shape[0] > sample_size:
+                df_2 = resample(df_2, replace=False, n_samples=sample_size, random_state=random_state)
+            if df_3.shape[0] > sample_size:
+                df_3 = resample(df_3, replace=False, n_samples=sample_size, random_state=random_state)
 
-    # 1.2 Keep the natural label distribution
-    seed_1, unlabeled_1, test_1 = partition_data(df_1, partition_ratio, sort_by_time)
-    seed_2, unlabeled_2, test_2 = partition_data(df_2, partition_ratio, sort_by_time)
-    seed_3, unlabeled_3, test_3 = partition_data(df_3, partition_ratio, sort_by_time)
-    seed, unlabeled, test = pd.concat([seed_1, seed_2, seed_3]), pd.concat([unlabeled_1, unlabeled_2, unlabeled_3]), pd.concat([test_1, test_2, test_3])
-    output['seed_size'], output['unlabeled_size'], output['test_size'] = seed.shape[0], unlabeled.shape[0], test.shape[0]
-    
-    initial_seed = seed.copy()
-    initial_unlabeled = unlabeled.copy()
-    
-    # 2. Train the model
-    initial_pipelines, initial_accuracy = train_model(initial_seed,2)
-    
-    # 3. Active Learning
-    if sampling_size == 0:
-        pipelines, accuracy = initial_pipelines, initial_accuracy
+        # 1.2 Keep the natural label distribution
+        seed_1, unlabeled_1, test_1 = partition_data(df_1, partition_ratio, sort_by_time)
+        seed_2, unlabeled_2, test_2 = partition_data(df_2, partition_ratio, sort_by_time)
+        seed_3, unlabeled_3, test_3 = partition_data(df_3, partition_ratio, sort_by_time)
+        seed, unlabeled, test = pd.concat([seed_1, seed_2, seed_3]), pd.concat([unlabeled_1, unlabeled_2, unlabeled_3]), pd.concat([test_1, test_2, test_3])
+        output['seed_size'], output['unlabeled_size'], output['test_size'] = seed.shape[0], unlabeled.shape[0], test.shape[0]
         
-    # 3.1 Initial Model + Random Sampling
-    elif training_method == 'random_sampling':
-        if initial_unlabeled.shape[0] >= sampling_size:
-            sample_unlabeled = initial_unlabeled.sample(n=sampling_size, replace=False, random_state=random_state)
+        initial_seed = seed.copy()
+        initial_unlabeled = unlabeled.copy()
+        
+        # 2. Train the model
+        initial_pipelines, initial_accuracy = train_model(initial_seed,2)
+        
+        # 3. Active Learning
+        if sampling_size == 0:
+            pipelines, accuracy = initial_pipelines, initial_accuracy
+            
+        # 3.1 Initial Model + Random Sampling
+        elif training_method == 'random_sampling':
+            if initial_unlabeled.shape[0] >= sampling_size:
+                sample_unlabeled = initial_unlabeled.sample(n=sampling_size, replace=False, random_state=random_state)
+            else:
+                sample_unlabeled = initial_unlabeled.sample(n=sampling_size, replace=True, random_state=random_state)
+            seed_and_sample_unlabeled_df = pd.concat([initial_seed, sample_unlabeled])
+            pipelines, accuracy = train_model(seed_and_sample_unlabeled_df,2)
+            
+        # 3.2 Initial Model + Active Learning
         else:
-            sample_unlabeled = initial_unlabeled.sample(n=sampling_size, replace=True, random_state=random_state)
-        seed_and_sample_unlabeled_df = pd.concat([initial_seed, sample_unlabeled])
-        pipelines, accuracy = train_model(seed_and_sample_unlabeled_df,2)
-        
-    # 3.2 Initial Model + Active Learning
-    else:
-        pipelines, accuracy = active_learning(initial_pipelines, initial_seed, initial_unlabeled, sampling_size,2)
+            pipelines, accuracy = active_learning(initial_pipelines, initial_seed, initial_unlabeled, sampling_size,2)
 
-    # 4. Report Model Accuracy
-    X_test, Y_test = test[['text_cleaned']], test[['SentimentScore']]
+        # 4. Report Model Accuracy
+        X_test, Y_test = test[['text_cleaned']], test[['SentimentScore']]
 
-    pl_preds = []
-    for pl in pipelines:
-        # compute the testing accuracy
-        prediction = pl.predict(pd.DataFrame(X_test))
-        pl_preds.append([pl, prediction])
+        pl_preds = []
+        for pl in pipelines:
+            # compute the testing accuracy
+            prediction = pl.predict(pd.DataFrame(X_test))
+            pl_preds.append([pl, prediction])
+            
+        #Saves all the model predictions
+        all_preds = np.array([x[1] for x in pl_preds]).transpose()
+        #Find the mode in all preds
+        prediction = [mode(i) for i in all_preds]
+        accuracy = round(accuracy_score(Y_test, prediction), round_number)
+        f1_micro = round(f1_score(np.array(Y_test), prediction, average='micro'), round_number)
+        f1_macro = round(f1_score(np.array(Y_test), prediction, average='macro'), round_number)
+        f1_weighted = round(f1_score(np.array(Y_test), prediction, average='weighted'), round_number)
         
-    #Saves all the model predictions
-    all_preds = np.array([x[1] for x in pl_preds]).transpose()
-    #Find the mode in all preds
-    prediction = [mode(i) for i in all_preds]
-    accuracy = round(accuracy_score(Y_test, prediction), round_number)
-    f1_micro = round(f1_score(np.array(Y_test), prediction, average='micro'), round_number)
-    f1_macro = round(f1_score(np.array(Y_test), prediction, average='macro'), round_number)
-    f1_weighted = round(f1_score(np.array(Y_test), prediction, average='weighted'), round_number)
+        precision_micro = round(precision_score(np.array(Y_test), prediction, average='micro'), round_number)
+        precision_macro = round(precision_score(np.array(Y_test), prediction, average='macro'), round_number)
+        precision_weighted = round(precision_score(np.array(Y_test), prediction, average='weighted'), round_number)
+            
+        recall_micro = round(recall_score(np.array(Y_test), prediction, average='micro'), round_number)
+        recall_macro = round(recall_score(np.array(Y_test), prediction, average='macro'), round_number)
+        recall_weighted = round(recall_score(np.array(Y_test), prediction, average='weighted'), round_number)
     
-    precision_micro = round(precision_score(np.array(Y_test), prediction, average='micro'), round_number)
-    precision_macro = round(precision_score(np.array(Y_test), prediction, average='macro'), round_number)
-    precision_weighted = round(precision_score(np.array(Y_test), prediction, average='weighted'), round_number)
-        
-    recall_micro = round(recall_score(np.array(Y_test), prediction, average='micro'), round_number)
-    recall_macro = round(recall_score(np.array(Y_test), prediction, average='macro'), round_number)
-    recall_weighted = round(recall_score(np.array(Y_test), prediction, average='weighted'), round_number)
-    
+        accuracy_lst.append(accuracy)
+        f1_micro_lst.append(f1_micro)
+        f1_macro_lst.append(f1_macro)
+        f1_weighted_lst.append(f1_weighted)
+        precision_micro_lst.append(precision_micro) 
+        precision_macro_lst.append(precision_macro) 
+        precision_weighted_lst.append(precision_weighted)
+        recall_micro_lst.append(recall_micro) 
+        recall_macro_lst.append(recall_macro) 
+        recall_weighted_lst.append(recall_weighted)
+
     output['accuracy'] = accuracy
     output['f1_micro'], output['f1_macro'], output['f1_weighted'] = f1_micro, f1_macro, f1_weighted
     output['precision_micro'], output['precision_macro'], output['precision_weighted'] = precision_micro, precision_macro, precision_weighted
